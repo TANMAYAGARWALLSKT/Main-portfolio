e313354801d20355b2cf58639e06fe9d84f1c4a6
TANMAYAGARWALLSKT <131980930+TANMAYAGARWALLSKT@users.noreply.github.com>
2024-03-16 19:49:33 +0530

    rrr

--- src/components/Page2.jsx
+++ src/components/Page2.jsx
@@ -10,7 +10,7 @@ function Page2() {
     // 08111a
       // data-scroll
       // data-scroll-speed=""
-      className="w-screen h-screen p-1 bg-[#08111a] text-zinc-200 rounded-[3rem]  overflow-hidden relative top-[90vh]  "
+      className="w-screen sm:h-[90vh] h-[80vh] p-1 bg-[#08111a] text-zinc-200 rounded-[3rem]  overflow-hidden relative sm:top-[90vh] top-[80vh] "
     >
       <div className="flex h-[23vh] overflow-hidden" id="text">
         <motion.div
