d21e9821c682ad23def357e6b77f3184273eb1d5
TANMAYAGARWALLSKT <131980930+TANMAYAGARWALLSKT@users.noreply.github.com>
2024-03-16 20:01:17 +0530

    rrr

--- src/components/Page3.jsx
+++ src/components/Page3.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Page3() {
   return (
-    <div className="w-full h-screen relative p-5 font-mono rounded-3xl top-[70vh] text-3xl bg-neutral-900 text-white">
+    <div data-scroll className="w-full h-screen relative p-5 font-mono rounded-3xl top-[70vh] text-3xl bg-neutral-900 text-white">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias laborum
       velit, quidem enim incidunt et recusandae sequi? Eveniet, reprehenderit
       dignissimos consequatur aspernatur beatae commodi corrupti, facere culpa
