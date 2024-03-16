ad96ad22433d68ced082e456ceb85db31f63280b
TANMAYAGARWALLSKT <131980930+TANMAYAGARWALLSKT@users.noreply.github.com>
2024-03-16 19:54:12 +0530

    ddd

--- src/components/Page3.jsx
+++ src/components/Page3.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Page3() {
   return (
-    <div className="w-full h-screen relative rounded-2xl top-[75vh] bg-neutral-900 text-white">
+    <div className="w-full h-screen relative rounded-3xl top-[70vh] bg-neutral-900 text-white">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias laborum
       velit, quidem enim incidunt et recusandae sequi? Eveniet, reprehenderit
       dignissimos consequatur aspernatur beatae commodi corrupti, facere culpa
