001be7ce294d50f0a9983e4bed0d5bc2827e9cc6
TANMAYAGARWALLSKT <131980930+TANMAYAGARWALLSKT@users.noreply.github.com>
2024-03-17 17:40:41 +0530

    fff

--- src/components/Page2.jsx
+++ src/components/Page2.jsx
@@ -15,7 +15,7 @@ function Page2() {
     >
       <div className="flex h-[23vh] overflow-hidden" id="text">
         <motion.div
-          className="flex flex-wrap lg:mt-10 sm:mt-2 md:mt-10  h-56 w-screen lg:text-8xl text-5xl md:text-5xl font-extrabold gap-10 "
+          className="flex flex-wrap lg:mt-10 sm:mt-2 md:mt-10 sm:h-[20vh] md:h-[15vh] lg-[15vh] w-screen lg:text-8xl text-5xl md:text-5xl font-extrabold gap-10 "
           id="div"
         >
           <Marquee className="overflow-hidden font-bold flex  tracking-wider font-mono opacity-20  ">
