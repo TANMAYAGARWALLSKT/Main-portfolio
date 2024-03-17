76e853f0764f394eeeaec41b87494970cb6bad32
TANMAYAGARWALLSKT <131980930+TANMAYAGARWALLSKT@users.noreply.github.com>
2024-03-17 17:53:18 +0530

    hghhg

--- src/components/Page2.jsx
+++ src/components/Page2.jsx
@@ -11,9 +11,9 @@ function Page2() {
       data-scroll
       className="w-screen sm:h-[90vh] h-[80vh] p-1 bg-[#08111a] text-zinc-200 rounded-[3rem]  overflow-hidden relative sm:top-[90vh] top-[85vh] "
     >
-      <div className="flex h-[23vh] overflow-hidden" id="text">
+      <div className="flex h-[25vh] overflow-hidden" id="text">
         <motion.div
-          className="flex flex-wrap lg:mt-10 sm:-mt-2 md:-mt-10 sm:h-[20vh] md:h-[18vh] lg-[18vh] w-screen lg:text-8xl text-5xl md:text-5xl font-extrabold gap-10 "
+          className="flex flex-wrap lg:mt-10 sm:-mt-2 md:-mt-10 sm:h-[25vh] md:h-[25vh] lg-[25] w-screen lg:text-8xl text-5xl md:text-5xl font-extrabold gap-10 "
           id="div"
         >
           <Marquee className="overflow-hidden font-bold flex  tracking-wider font-mono opacity-20  ">
